c9a0e8bb9cb954e7dcf6505a1004142edf725db7
Andriy Yushchenko <andriy.yushchenko97@gmail.com>
2021-12-26 07:04:10 +0200

    add area hints

--- BookShopWeb/Areas/Admin/Controllers/CoverTypeController.cs
+++ BookShopWeb/Areas/Admin/Controllers/CoverTypeController.cs
@@ -2,112 +2,112 @@ using BookShop.DataAccess.Repository.IRepository;
 using BookShop.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BookShopWeb.Areas.Admin.Controllers
+namespace BookShopWeb.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class CoverTypeController : Controller
 {
-    public class CoverTypeController : Controller
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CoverTypeController(IUnitOfWork unitOfWork)
     {
-        private readonly IUnitOfWork _unitOfWork;
-
-        public CoverTypeController(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
+        _unitOfWork = unitOfWork;
+    }
         
-        public ActionResult Index()
+    public ActionResult Index()
+    {
+        var coverTypes = _unitOfWork.CoverType.GetAll();
+        return View(coverTypes);
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(CoverType coverType)
+    {
+        if (!ModelState.IsValid)
         {
-            var coverTypes = _unitOfWork.CoverType.GetAll();
-            return View(coverTypes);
-        }
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(CoverType coverType)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(coverType);
-            }
-
-            _unitOfWork.CoverType.Add(coverType);
-            _unitOfWork.Save();
-
-            TempData["success"] = "Cover Type added successfully";
-
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult Edit(int? id)
-        {
-            if (id is null)
-            {
-                return NotFound();
-            }
-
-            var coverType = _unitOfWork.CoverType.FirstOrDefault(coverType => coverType.Id == id);
-            if (coverType == null)
-            {
-                return NotFound();
-            }
-            
             return View(coverType);
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(CoverType coverType)
+        _unitOfWork.CoverType.Add(coverType);
+        _unitOfWork.Save();
+
+        TempData["success"] = "Cover Type added successfully";
+
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Edit(int? id)
+    {
+        if (id is null)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(coverType);
-            }
-
-            _unitOfWork.CoverType.Update(coverType);
-            _unitOfWork.Save();
-
-            TempData["success"] = "Cover Type updated successfully";
-
-            return RedirectToAction("Index");
+            return NotFound();
         }
 
-        // GET: CoverType/Delete/5
-        public ActionResult Delete(int? id)
+        var coverType = _unitOfWork.CoverType.FirstOrDefault(coverType => coverType.Id == id);
+        if (coverType == null)
         {
-            if (id is null)
-            {
-                return NotFound();
-            }
-
-            var coverType = _unitOfWork.CoverType.FirstOrDefault(coverType => coverType.Id == id);
-            if (coverType == null)
-            {
-                return NotFound();
-            }
+            return NotFound();
+        }
             
+        return View(coverType);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(CoverType coverType)
+    {
+        if (!ModelState.IsValid)
+        {
             return View(coverType);
         }
 
-        // POST: CoverType/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id)
+        _unitOfWork.CoverType.Update(coverType);
+        _unitOfWork.Save();
+
+        TempData["success"] = "Cover Type updated successfully";
+
+        return RedirectToAction("Index");
+    }
+
+    // GET: CoverType/Delete/5
+    public ActionResult Delete(int? id)
+    {
+        if (id is null)
         {
-            var coverType = _unitOfWork.CoverType.FirstOrDefault(cat => cat.Id == id);
-
-            if (coverType == null)
-            {
-                return NotFound();
-            }
-            _unitOfWork.CoverType.Remove(coverType);
-            _unitOfWork.Save();
-
-            TempData["success"] = "Cover Type removed successfully";
-
-            return RedirectToAction("Index");
+            return NotFound();
         }
+
+        var coverType = _unitOfWork.CoverType.FirstOrDefault(coverType => coverType.Id == id);
+        if (coverType == null)
+        {
+            return NotFound();
+        }
+            
+        return View(coverType);
+    }
+
+    // POST: CoverType/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Delete(int id)
+    {
+        var coverType = _unitOfWork.CoverType.FirstOrDefault(cat => cat.Id == id);
+
+        if (coverType == null)
+        {
+            return NotFound();
+        }
+        _unitOfWork.CoverType.Remove(coverType);
+        _unitOfWork.Save();
+
+        TempData["success"] = "Cover Type removed successfully";
+
+        return RedirectToAction("Index");
     }
 }
--- BookShopWeb/Areas/Admin/Controllers/ProductController.cs
+++ BookShopWeb/Areas/Admin/Controllers/ProductController.cs
@@ -10,145 +10,145 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace BookShopWeb.Areas.Admin.Controllers
+namespace BookShopWeb.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class ProductController : Controller
 {
-    public class ProductController : Controller
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IWebHostEnvironment _hostEnvironment;
+
+    public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
     {
-        private readonly IUnitOfWork _unitOfWork;
-        private readonly IWebHostEnvironment _hostEnvironment;
-
-        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
-        {
-            _unitOfWork = unitOfWork;
-            _hostEnvironment = hostEnvironment;
-        }
+        _unitOfWork = unitOfWork;
+        _hostEnvironment = hostEnvironment;
+    }
         
-        public ActionResult Index()
-        {
-            return View();
-        }
+    public ActionResult Index()
+    {
+        return View();
+    }
 
-        public ActionResult Upsert(int? id)
+    public ActionResult Upsert(int? id)
+    {
+        var productViewModel = new ProductViewModel()
         {
-            var productViewModel = new ProductViewModel()
-            {
-                Product = new Product(),
-                CategoryList = _unitOfWork.Category.GetAll()
-                    .Select(category => new SelectListItem(
-                            category.Name,
-                            category.Id.ToString()
-                        )
-                    ),
-                CoverTypeList = _unitOfWork.CoverType.GetAll()
-                    .Select(coverType => new SelectListItem(
-                            coverType.Name,
-                            coverType.Id.ToString()
-                        )
+            Product = new Product(),
+            CategoryList = _unitOfWork.Category.GetAll()
+                .Select(category => new SelectListItem(
+                        category.Name,
+                        category.Id.ToString()
                     )
-            };
+                ),
+            CoverTypeList = _unitOfWork.CoverType.GetAll()
+                .Select(coverType => new SelectListItem(
+                        coverType.Name,
+                        coverType.Id.ToString()
+                    )
+                )
+        };
 
-            if (id is not (null or 0))
-            {
-                var product = _unitOfWork.Product.FirstOrDefault(product => product.Id == id);
-
-                if (product is null)
-                {
-                    return NotFound();
-                }
-
-                productViewModel.Product = product;
-            }
-
-            return View(productViewModel);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Upsert(ProductViewModel productViewModel, IFormFile? file)
+        if (id is not (null or 0))
         {
-            if (!ModelState.IsValid)
-            {
-                return View(productViewModel);
-            }
+            var product = _unitOfWork.Product.FirstOrDefault(product => product.Id == id);
 
-            if (file != null)
-            {
-                var wwwRootPath = _hostEnvironment.WebRootPath;
-                var fileName = Guid.NewGuid().ToString();
-                var uploads = Path.Combine(wwwRootPath, @"images\products");
-                var extension = Path.GetExtension(file.FileName);
-
-                if (productViewModel.Product.ImageUrl is not null)
-                {
-                    var oldImagePath = Path.Combine(wwwRootPath, productViewModel.Product.ImageUrl.TrimStart('\\'));
-                    if (System.IO.File.Exists(oldImagePath))
-                    {
-                        System.IO.File.Delete(oldImagePath);
-                    }
-                }
-
-                using (var fileStream = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
-                {
-                    file.CopyTo(fileStream);
-                }
-
-                productViewModel.Product.ImageUrl = @$"\images\products\{fileName}{extension}";
-            }
-
-            if (productViewModel.Product.Id is 0)
-            {
-                _unitOfWork.Product.Add(productViewModel.Product);    
-            }
-            else
-            {
-                _unitOfWork.Product.Update(productViewModel.Product);
-            }
-            
-            _unitOfWork.Save();
-
-            TempData["success"] = "Product added successfully";
-
-            return RedirectToAction("Index");
-        }
-
-        #region API Controller
-
-        [HttpGet]
-        public IActionResult GetAll()
-        {
-            var productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
-
-            return Ok(new
-            {
-                data = productList
-            });
-        }
-        
-        [HttpDelete]
-        public ActionResult Delete(int? id)
-        {
-            var product = _unitOfWork.Product.FirstOrDefault(cat => cat.Id == id);
-
-            if (product == null)
+            if (product is null)
             {
                 return NotFound();
             }
 
-
-            Debug.Assert(product.ImageUrl != null, "product.ImageUrl != null");
-            var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, product.ImageUrl.TrimStart('\\'));
-            if (System.IO.File.Exists(oldImagePath))
-            {
-                System.IO.File.Delete(oldImagePath);
-            }
-            _unitOfWork.Product.Remove(product);
-            _unitOfWork.Save();
-
-            TempData["success"] = "Cover Type removed successfully";
-
-            return Ok();
+            productViewModel.Product = product;
         }
 
-        #endregion
+        return View(productViewModel);
     }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Upsert(ProductViewModel productViewModel, IFormFile? file)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(productViewModel);
+        }
+
+        if (file != null)
+        {
+            var wwwRootPath = _hostEnvironment.WebRootPath;
+            var fileName = Guid.NewGuid().ToString();
+            var uploads = Path.Combine(wwwRootPath, @"images\products");
+            var extension = Path.GetExtension(file.FileName);
+
+            if (productViewModel.Product.ImageUrl is not null)
+            {
+                var oldImagePath = Path.Combine(wwwRootPath, productViewModel.Product.ImageUrl.TrimStart('\\'));
+                if (System.IO.File.Exists(oldImagePath))
+                {
+                    System.IO.File.Delete(oldImagePath);
+                }
+            }
+
+            using (var fileStream = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+
+            productViewModel.Product.ImageUrl = @$"\images\products\{fileName}{extension}";
+        }
+
+        if (productViewModel.Product.Id is 0)
+        {
+            _unitOfWork.Product.Add(productViewModel.Product);    
+        }
+        else
+        {
+            _unitOfWork.Product.Update(productViewModel.Product);
+        }
+            
+        _unitOfWork.Save();
+
+        TempData["success"] = "Product added successfully";
+
+        return RedirectToAction("Index");
+    }
+
+    #region API Controller
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+
+        return Ok(new
+        {
+            data = productList
+        });
+    }
+        
+    [HttpDelete]
+    public ActionResult Delete(int? id)
+    {
+        var product = _unitOfWork.Product.FirstOrDefault(cat => cat.Id == id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+
+        Debug.Assert(product.ImageUrl != null, "product.ImageUrl != null");
+        var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, product.ImageUrl.TrimStart('\\'));
+        if (System.IO.File.Exists(oldImagePath))
+        {
+            System.IO.File.Delete(oldImagePath);
+        }
+        _unitOfWork.Product.Remove(product);
+        _unitOfWork.Save();
+
+        TempData["success"] = "Cover Type removed successfully";
+
+        return Ok();
+    }
+
+    #endregion
 }
